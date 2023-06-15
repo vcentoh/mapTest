e0b5e931b98bf3b970f3d0f958c65ba5c4113ab3
vcentoh <vlncent.afm@gmail.com>
2023-06-15 14:53:30 -0400

    Initial Commit

---
+++ RidderyMap/RidderyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidderyMapApp.swift
+//  RidderyMap
+//
+//  Created by Magik on 15/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RidderyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
